028f63047093d18b2a205dc682b4dfc4602255f5
OmkarParekh <omkarparekh600@gmail.com>
2020-12-22 22:04:37 +0530

    ghgfhfgh

--- routes/login.js
+++ routes/login.js
@@ -12,11 +12,11 @@ router.post('/',(req,res)=>{
      .then(login=>{
           if(login===null)
           {
-               res.sendStatus(401).send({
+               res.send({
                     "Status":'UnSuccess',
                     "data":'UnAuthorized'
                    
-               })
+               }).sendStatus(401)
           }
           else{
                jwt.sign({"Username":login.Username,"Password":login.Password},process.env.jwt_code,(err,tk)=>{

5ab9bccebd2ad542f2a694ef0b2d727cf4b85015
OmkarParekh <omkarparekh600@gmail.com>
2020-12-22 21:09:42 +0530

    gfhfghfg

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,30 @@
 const router=require('express').Router()
 const jwt=require('jsonwebtoken')
-router.get('/:Username/:Password',(req,res)=>{
-     jwt.sign({"Username":req.params.Username,"Password":req.params.Password},process.env.jwt_code,(err,tk)=>{
-          if(err) throw res.send(err)
-          res.send({
-              "token":tk
+const mongo=require('mongoose')
+const conn=mongo.createConnection(process.env.mongo_conn,{ useNewUrlParser: true,useUnifiedTopology: true})
+router.get('/',(req,res)=>{
+     const data=req.body
+     require('../models/Usersigindata')
+     const User=conn.model('user')
+     User.findOne({"Username":data.Username,"Password":data.Password})
+     .then(res=>{
+          jwt.sign({"Username":data.Username,"Password":data.Password},process.env.jwt_code,(err,tk)=>{
+               if(err) throw res.send(err)
+               res.send({
+                   "token":tk,
+                   "Status":'Success'
+               })
+               
           })
-          
      })
+     .catch(err=>{
+          res.sendStatus(401).send({
+               "Status":'Wrong Password'
+          })
+     })
+
+
+     
    
 })
 module.exports=router;

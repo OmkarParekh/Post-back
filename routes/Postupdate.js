46165e3e3a695e608e700d71f8f5fcaedb79dcc5
OmkarParekh <omkarparekh600@gmail.com>
2021-01-19 15:16:17 +0530

    dfdfsdfsd

--- routes/Postupdate.js
+++ routes/Postupdate.js
@@ -3,7 +3,7 @@ const mongo=require('mongoose')
 const conn=mongo.createConnection(process.env.mongo_conn,{ useNewUrlParser: true,useUnifiedTopology: true})
 require('../models/post')
 const Post=conn.model('post')
-const {postupdateauth}=require('../Authentication/authenroute')
+const {postupdateauth, NormalAuth}=require('../Authentication/authenroute')
 
 router.post('/:id',postupdateauth,(req,res)=>{
      const data=req.body
@@ -14,6 +14,13 @@ router.post('/:id',postupdateauth,(req,res)=>{
           Description:data.Description,
           Date:data.Date
      })
+     .then(data=>{
+          res.send(data)
+     })
+})
+router.get('/fetch/:id',NormalAuth,(req,res)=>{
+    
+     Post.findOne({_id:req.params.id})
      .then(data=>{
           res.send(data)
      })
